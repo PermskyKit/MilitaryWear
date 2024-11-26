74333a67d22d95b2b36c4cb4cf92f97589519594
PermskyKit <n17permyakov@gmail.com>
2024-11-26 16:57:40 +0300

    Just a Git test~

--- src/main/java/com/permskykit/militarywear/MilitaryWear.java
+++ src/main/java/com/permskykit/militarywear/MilitaryWear.java
@@ -29,6 +29,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
+// Okay, go Git
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MilitaryWear.MODID)
 public class MilitaryWear
